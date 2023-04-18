ee0369f97a3be1b515c105f9f1c9a451859da38e
David Enke <david.enke@zalari.de>
2023-04-18 09:38:48 +0200

    force ua in playwright

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,8 @@ const {
 const browser = await chromium.launch();
 const context = await browser.newContext({
   baseURL: baseUrl,
+  userAgent:
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
 });
 await context.route('**.jpg*', (route) => route.abort());
 
